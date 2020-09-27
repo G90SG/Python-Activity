878524e9b0e92d011e287cc1a3d77b6337f872d6
Gemma Dingwall <gemma9261@hotmail.com>
2020-09-27 16:06:38 +0000

    file names

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
+weather = "raining"
+if weather == "raining":
+  print ("take umbrella")
+else : 
+  print("wear coat")
 
-name = input ("What is your name?")
-print (name)
-print ("Hello" + name)
 
-city = input ("Which city do you live in?")
-print (city)
-print ("Birmingham is the second biggest city")
 
-#page 21#
-product_name = input ("What is the product Name")
-product_price = float(input("What is the product price?"))
-product_quantity = int(input("How many products are there?"))
-total_price = product_quantity * product_price 
-# print("Total Price: " + total_price)  what have i missed here?#
+
+
 
 
 
