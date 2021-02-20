53824ad96b649b1f2fe9eb115ce64d49eac05a63
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-20 17:20:39 +0000

    Update and rename Page24.py to Activity 3.py

---
+++ Activity 3.py
@@ -0,0 +1,22 @@
+# Create a program to ask the user the weather and advise them what clothing to take.
+
+#Assign weather variables 
+raining = "a"
+cold = "b"
+fine = "c"
+sunny = "d"
+# Ask the user what the weather is like today
+print ("What is the weather like today?")
+weather = input("Is it (a) Raining  (b) Cold  (c) Fine (d) Sunny? ")
+
+# Create a variable for weather and assign the string "raining" 
+if (weather == raining):
+  print ("Don't forget an umbrella!")
+elif (weather == cold):
+  print ("Don't forget to take a coat!")
+elif (weather == fine):
+  print ("Lovely day for a walk!")
+elif (weather == sunny):
+  print ("Suns out Guns out! ")
+else:
+  print ("That isn't the answer I was looking for!")
