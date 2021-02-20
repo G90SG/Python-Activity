7f5552e6556be3caa699923623f9da02cab4961a
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-20 16:55:12 +0000

    Update and rename Page15.py to Activities.py

---
+++ Activities.py
@@ -0,0 +1,63 @@
+# Use and print various mathematical operators
+print ("2 + 2 + 2 + 2 + 2")
+print ("=")
+print(2+2+2+2+2)
+print ("\n")
+
+print ("8 x 6 ")
+print ("=")
+print(8*6)
+print ("\n")
+
+print ("10 - 5 + 6 ")
+print ("=")
+print(10-5+6)
+print ("\n")
+
+print ("2 + 2 ")
+print ("=")
+print(2+2) 
+print ("\n")
+
+print ("10 % 2")
+print ("=")
+print(10/2)
+
+#print new line
+print("\n")
+
+# When salary is 200, what is salary + 50 , salary + salary, salary * salary?
+salary = 200
+print ("My Salary is £",salary ) 
+print ("If I add £50 to my Salary")
+salary = salary + 50
+print ("My Salary would be £",salary)
+print("\n")
+salary = salary + salary
+print ("If I add my Salary to my Salary")
+print ("My Salary would be £", salary)
+print("\n")
+salary = salary * salary
+print ("If I multiply my Salary by itself")
+print ("My Salary would be £", salary)
+
+print("\n")
+# If price is 10 and stock is 5 - what is the total of price * stock
+price = 10
+print ("The price is £", price)
+stock = 5
+print ("There are", stock , "items of stock")
+print("\n")
+plus = price + stock
+print ("The total of Price + Stock is", plus)
+print("\n")
+total = price * stock
+print ("The total price of the stock is £", total) 
+
+print("\n")
+#assigning a variable and printing 
+product_name = 'iPhone'
+product_price = 450
+print (product_name)
+print (product_price)
+print (product_name , product_price)
