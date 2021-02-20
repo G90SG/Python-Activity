07022c0fc649ad366a11e5c88699136181f1d97b
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-20 15:54:59 +0000

    Update Page20.py

--- Page20.py
+++ Page20.py
@@ -1,15 +1,8 @@
 
-name = input ("What is your name?")
-print (name)
-print ("Hello" + name)
 
-city = input ("Which city do you live in?")
-print (city)
-print ("Birmingham is the second biggest city")
-
-#page 21#
-product_name = input ("What is the product Name")
-product_price = float(input("What is the product price?"))
-product_quantity = int(input("How many products are there?"))
-total_price = product_quantity * product_price 
-# print("Total Price: " + total_price)  what have i missed here?#
+# Create variables for Name and City - get input from the user and concatenate the variable and the sentence
+name = input ("What is your name? ")
+city = input ("Which city do you live in, " + name + "? ")
+# Print new line
+print ("\n")
+print (city + "'s my favourite City!")
