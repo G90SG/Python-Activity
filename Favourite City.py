223358cab3023d943985f84a057c800fd762b05a
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-21 09:31:54 +0000

    Update Favourite City.py

--- Favourite City.py
+++ Favourite City.py
@@ -1,8 +1,7 @@
-
-
-# Create variables for Name and City - get input from the user and concatenate the variable and the sentence
+# Create variables for Name and City - get input from the user and concatenate the variable within a string 
 name = input ("What is your name? ")
+print ("\n")
 city = input ("Which city do you live in, " + name + "? ")
 # Print new line
 print ("\n")
-print (city + "'s my favourite City!")
+print (city + " is my favourite City!")
