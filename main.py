84c8e2ec5bebda88e46e4ecdad43683cdc827889
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-21 09:21:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+# Create a program to ask the user their name, ask them guess the number I am thinking of between 1 and 10
+
+# Import random module
 import random
-myName = input ("Hello! What is your name?")
+# Say Hello and get the users name
+myName = input ("Hello! What is your name? ")
+# Assign the variable 'number' to a random integer between 1 and 10
 number = random.randint(1,10)
+# Tell the user I am thinking of a number between 1 and 10
 print ("Well, " + myName + " I am thinking of a number between 1 and 10.")
-guess = int(input( "Take a guess:"))
+# Take the guess from the users keyboard input 
+guess = int(input( "Take a guess: "))
 if guess == number: 
   print("Good job, " + myName + "! You guessed my number")
+if guess != number:
+  print ("Better luck next time.")
