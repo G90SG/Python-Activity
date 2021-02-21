6746146622a28c4a910fe99b17b9674c3007c2a8
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-21 09:35:22 +0000

    Update Number Guess.py

--- Number Guess.py
+++ Number Guess.py
@@ -7,10 +7,10 @@ myName = input ("Hello! What is your name? ")
 # Assign the variable 'number' to a random integer between 1 and 10
 number = random.randint(1,10)
 # Tell the user I am thinking of a number between 1 and 10
-print ("Well, " + myName + " I am thinking of a number between 1 and 10.")
+print (myName + ", I am thinking of a number between 1 and 10.")
 # Take the guess from the users keyboard input 
 guess = int(input( "Take a guess: "))
 if guess == number: 
-  print("Good job, " + myName + "! You guessed my number")
+  print("Good job, " + myName + "! You guessed my number.")
 if guess != number:
   print ("Better luck next time.")
